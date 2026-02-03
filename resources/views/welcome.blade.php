7aefeaa6b0c702b0a9b29b1bf442047bb69a1300
RustyCLopez <lopezcortezrusty2004@gmail.com>
2026-02-03 18:53:06 +0800

    rusty lopez

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+
+        
+        <style>
+            body {
+                font-family: 'Figtree', sans-serif;
+            }
+        </style>
+    </head>
+    <body class="antialiased">
+        <div class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-gray-100 dark:bg-gray-900 selection:bg-red-500 selection:text-white">
+            <div class="max-w-7xl mx-auto p-6 lg:p-8">
+
+                <div class="flex justify-center">
+                    <h1 class="text-4xl font-bold text-gray-900 dark:text-white">
+                        
+                    </h1>
+                </div>
+
+                
+                <div class="flex justify-center mt-4">
+                    <h2 style="color: blue;">
+                        Rusty Lopez
+                    </h2>
+                </div>
+
+            </div>
+        </div>
+    </body>
+</html>
